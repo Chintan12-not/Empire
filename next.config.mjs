a740f6b312e1de8c4aec52f13c3f5630ca3d49c8
Chintan Maheshwari <chintanmaheshwari13286@gmail.com>
2026-09-05 20:46:05 +0530

    Add root & public admin-secret.html and vercel.json for Vercel 404 resolution

--- next.config.mjs
+++ next.config.mjs
@@ -5,20 +5,6 @@ const nextConfig = {
     unoptimized: true,
     domains: ['localhost', 'wolxccbehsbafyirgvgp.supabase.co'],
   },
-  async redirects() {
-    return [
-      {
-        source: '/admin-secret.html',
-        destination: '/admin',
-        permanent: false,
-      },
-      {
-        source: '/admin-secret',
-        destination: '/admin',
-        permanent: false,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
