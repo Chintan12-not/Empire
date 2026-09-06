661e666bb20723fd7e92dfabb1322710cbfcfba5
Chintan Maheshwari <chintanmaheshwari13286@gmail.com>
2026-09-06 22:43:00 +0530

    Update default bottles sold to 600 and recalculate sales metrics

--- lib/salesService.ts
+++ lib/salesService.ts
@@ -22,7 +22,7 @@ const STORAGE_KEY = 'empire_sales_performance_v1';
 
 export const DEFAULT_SALES_PERFORMANCE: SalesPerformanceInput = {
   salesPeriodMonths: 3,
-  totalBottlesSold: 1500,
+  totalBottlesSold: 600,
   pricePerBottle: 1170,
 };
 
